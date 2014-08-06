6a243b0705e3b68aa68636c0669773265a77574d
aalmiray <aalmiray@gmail.com>
2014-08-06 17:27:40 +0200

    First commit

---
+++ subprojects/griffon-icomoonicons-javafx-groovy/src/main/groovy/griffon/builder/javafx/factory/IcoMoonIconFactory.groovy
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.builder.javafx.factory
+
+import griffon.javafx.support.icomoonicons.IcoMoonIcon
+import griffon.plugins.icomoonicons.IcoMoon
+import groovyx.javafx.factory.LabeledFactory
+
+/**
+ * @author Andres Almiray
+ */
+public class IcoMoonIconFactory extends LabeledFactory {
+    IcoMoonIconFactory() {
+        super(IcoMoonIcon)
+    }
+
+    public Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
+        def icon = attributes.remove('icon') ?: value
+        int size = attributes.remove('size') ?: 16
+
+        if (icon instanceof IcoMoon) {
+            return new IcoMoonIcon(icon, size)
+        } else if (icon instanceof CharSequence) {
+            return new IcoMoonIcon(IcoMoon.findByDescription(icon.toString()), size)
+        }
+
+        throw new IllegalArgumentException("In $name you must define a node value or icon:")
+    }
+}
---
+++ subprojects/griffon-icomoonicons-swing/src/main/java/griffon/swing/support/icomoonicons/IcoMoonIcon.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.swing.support.icomoonicons;
+
+import griffon.plugins.icomoonicons.IcoMoon;
+
+import javax.annotation.Nonnull;
+import javax.swing.ImageIcon;
+import java.net.URL;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author Andres Almiray
+ */
+public class IcoMoonIcon extends ImageIcon {
+    private final IcoMoon icomoon;
+    private final int size;
+
+    public IcoMoonIcon(@Nonnull IcoMoon icomoon) {
+        this(icomoon, 16);
+    }
+
+    public IcoMoonIcon(@Nonnull IcoMoon icomoon, int size) {
+        super(toURL(icomoon, size));
+        this.icomoon = icomoon;
+        this.size = size;
+    }
+
+    public IcoMoonIcon(@Nonnull String description) {
+        this(IcoMoon.findByDescription(description));
+    }
+
+    @Nonnull
+    private static URL toURL(@Nonnull IcoMoon icomoon, int size) {
+        requireNonNull(icomoon, "Argument 'icomoon' must not be null.");
+        String resource = icomoon.asResource(size);
+        return Thread.currentThread().getContextClassLoader().getResource(resource);
+    }
+
+    @Nonnull
+    public IcoMoon getIcoMoon() {
+        return icomoon;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
